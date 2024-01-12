0b20886a990b442a83856a8c9e63a4259593d48e
Matthew Kim <matthewkimtx@gmail.com>
2024-01-12 15:52:21 +0800

    login, signup, and query are functional

--- mongoBucket/selectApp/views.py
+++ mongoBucket/selectApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from dotenv import load_dotenv
 from pymongo import MongoClient
-from mongo import cleanOutput
+from api_account_password.mongo import cleanOutput
 import os
 
 load_dotenv
